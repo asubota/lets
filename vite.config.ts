660b0b003046fddae5855401a10c130dd5bc0390
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-10 16:20:26 +0300

    github pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
+const base = 'lets'
+
 export default defineConfig({
-  plugins: [react()],
+  base,
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: ['@mui/material'],
+        },
+      },
+    },
+  },
+  plugins: [react(), basicSsl()],
   server: {
     port: 8000,
   },
