013bfd4b4d701005e3be508c6f0b4ba5877f9db5
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-14 20:12:44 +0300

    gradient body

--- src/shell.tsx
+++ src/shell.tsx
@@ -6,7 +6,7 @@ export const Shell: FC = () => {
   const [search, setSearch] = useState('')
 
   return (
-    <Box sx={{ p: 1, pt: 0 }}>
+    <Box sx={{ p: 1, pt: 0 }} className="bg">
       <SearchField onSubmit={({ input }) => setSearch(input)} />
       <List search={search} />
     </Box>
