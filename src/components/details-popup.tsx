ded7d0b3f4a21ea0a1762337dbdb6442c4907b20
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-15 15:48:52 +0300

    better modal ui

--- src/components/details-popup.tsx
+++ src/components/details-popup.tsx
@@ -1,7 +1,15 @@
-import { Dialog, DialogContent, DialogTitle, Typography } from '@mui/material'
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  Link,
+  Typography,
+} from '@mui/material'
 import { FC } from 'react'
 import { Product } from '../types.ts'
 import { ImageSlider } from './image-slider.tsx'
+import LinkIcon from '@mui/icons-material/Link'
 
 export const DetailsPopup: FC<{ details: Product; onClose: () => void }> = ({
   details,
@@ -9,15 +17,32 @@ export const DetailsPopup: FC<{ details: Product; onClose: () => void }> = ({
 }) => {
   return (
     <Dialog open={!!details} onClose={onClose}>
-      <DialogTitle typography="subtitle2" textAlign="center">
+      <DialogTitle typography="subtitle2" textAlign="left" sx={{ p: 1 }}>
         {details.name}
       </DialogTitle>
-      <DialogContent>
+
+      <Divider sx={{ ml: 1, mr: 1, borderColor: 'primary.main' }} />
+
+      <DialogContent sx={{ p: 1, minHeight: '200px' }}>
         {details.link && (
-          <Typography gutterBottom>
-            <a href={details.link} target="_blank" rel="noopener noreferrer">
-              Open vendor
-            </a>
+          <Typography
+            textAlign="center"
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: '10px',
+              justifyContent: 'center',
+            }}
+          >
+            <LinkIcon fontSize="small" sx={{ color: 'primary.main' }} />
+            <Link
+              href={details.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              sx={{ textDecoration: 'none' }}
+            >
+              Visit {details.vendor}
+            </Link>
           </Typography>
         )}
 
