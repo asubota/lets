4368c98e95866aeefbe99c50972aaa09e42fb25d
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-22 20:18:18 +0300

    table columns

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,15 +1,20 @@
 import { FC, useState } from 'react'
-import { Box, Switch, Typography } from '@mui/material'
+import { Box, IconButton, Switch, Typography } from '@mui/material'
 import { TilesView } from './tiles-view.tsx'
 import { TableView } from './table-view.tsx'
 import { useSearch } from '../use-data.ts'
 import { Welcome } from './welcome.tsx'
 import { NoResults } from './no-results.tsx'
+import SettingsIcon from '@mui/icons-material/Settings'
+import { useTableSettings } from './use-table-settings.ts'
+import { TableSettings } from './table-settings.tsx'
 
 export const List: FC<{ search: string }> = ({ search }) => {
   const [value, setValue] = useState(true)
   const list = useSearch(search)
   const View = value ? TilesView : TableView
+
+  const { open, toggle } = useTableSettings()
 
   if (list.length === 0 && search.length === 0) {
     return <Welcome />
@@ -38,9 +43,17 @@ export const List: FC<{ search: string }> = ({ search }) => {
         >
           Total: {list.length}
         </Typography>
+
+        {!value && (
+          <IconButton onClick={toggle}>
+            <SettingsIcon />
+          </IconButton>
+        )}
+
         <Switch value={value} onChange={(_, checked) => setValue(!checked)} />
       </Box>
 
+      <TableSettings open={open} handleClose={toggle} />
       <View list={list} search={search} />
     </>
   )
