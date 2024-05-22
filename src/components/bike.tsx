0489c9eb792267384c7198812e228f5cce590685
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-22 18:52:24 +0300

    small refactoring for bike and broken bike svg

---
+++ src/components/bike.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+import { Box } from '@mui/material'
+import SafeBike from './bike.svg?react'
+import BrokenBike from './broken-bike.svg?react'
+
+export const Bike: FC<{ type: 'safe' | 'broken' }> = ({ type }) => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: '300px',
+        position: 'absolute',
+        bottom: '185px',
+        left: '50%',
+        transform: 'translate(-50%)',
+      }}
+    >
+      {type === 'safe' && <SafeBike />}
+      {type === 'broken' && <BrokenBike />}
+    </Box>
+  )
+}
