2945c70ccb231eb1ea0ceab541935b0fc96e49e7
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-05-26 21:45:21 +0300

    show table settings

--- src/store/table.ts
+++ src/store/table.ts
@@ -4,8 +4,10 @@ import { Product } from '../types.ts'
 
 interface TableState {
   columns: (keyof Product)[]
+  showSettings: boolean
   actions: {
     setColumns(this: void, value: (keyof Product)[]): void
+    toggleSettings(this: void): void
   }
 }
 
@@ -13,8 +15,11 @@ const useTableStore = create<TableState>()(
   persist(
     (set) => ({
       columns: [],
+      showSettings: false,
       actions: {
         setColumns: (columns) => set(() => ({ columns })),
+        toggleSettings: () =>
+          set((state) => ({ showSettings: !state.showSettings })),
       },
     }),
     {
@@ -26,3 +31,5 @@ const useTableStore = create<TableState>()(
 
 export const useTableActions = () => useTableStore((state) => state.actions)
 export const useTableColumns = () => useTableStore((state) => state.columns)
+export const useShowTableSettings = () =>
+  useTableStore((state) => state.showSettings)
