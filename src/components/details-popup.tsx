e46a7b9d2e3c0ffb62ac1a732d6e56614fb8f78b
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-06-27 13:59:51 +0300

    details popup

--- src/components/details-popup.tsx
+++ src/components/details-popup.tsx
@@ -4,12 +4,15 @@ import {
   DialogTitle,
   Divider,
   Link,
-  Typography,
 } from '@mui/material'
 import { FC } from 'react'
 import { Product } from '../types.ts'
 import { ImageSlider } from './image-slider.tsx'
 import LinkIcon from '@mui/icons-material/Link'
+
+const getHref = (link: string): string => {
+  return link.startsWith('http') ? link : `https://${link}`
+}
 
 export const DetailsPopup: FC<{ details: Product; onClose: () => void }> = ({
   details,
@@ -17,39 +20,40 @@ export const DetailsPopup: FC<{ details: Product; onClose: () => void }> = ({
 }) => {
   return (
     <Dialog open={true} onClose={onClose}>
-      <DialogTitle typography="subtitle2" textAlign="left" sx={{ p: 1 }}>
-        {details.name}
+      <DialogTitle
+        typography="subtitle2"
+        textAlign="left"
+        sx={{
+          p: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          gap: '6px',
+        }}
+      >
+        {details.link && (
+          <>
+            <LinkIcon fontSize="small" sx={{ color: 'primary.main' }} />
+            <Link
+              title={details.name}
+              href={getHref(details.link)}
+              target="_blank"
+              rel="noopener noreferrer"
+              sx={{ textDecoration: 'none' }}
+              typography="subtitle2"
+              color="text.primary"
+            >
+              {details.name}
+            </Link>
+          </>
+        )}
+
+        {!details.link && details.name}
       </DialogTitle>
 
       <Divider sx={{ ml: 1, mr: 1, borderColor: 'primary.main' }} />
 
       <DialogContent sx={{ p: 1, pl: '2px', pr: '2px', minHeight: '250px' }}>
-        {details.link && (
-          <Typography
-            textAlign="center"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '10px',
-              justifyContent: 'center',
-            }}
-          >
-            <LinkIcon fontSize="small" sx={{ color: 'primary.main' }} />
-            <Link
-              href={
-                details.link.startsWith('http')
-                  ? details.link
-                  : `https://${details.link}`
-              }
-              target="_blank"
-              rel="noopener noreferrer"
-              sx={{ textDecoration: 'none' }}
-            >
-              Visit {details.vendor}
-            </Link>
-          </Typography>
-        )}
-
         {details.pics && <ImageSlider pics={details.pics} alt={details.name} />}
       </DialogContent>
     </Dialog>
