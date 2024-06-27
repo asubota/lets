d2765cfd1a83b5eeb3a461b793edf1c07166bf93
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-06-27 23:45:31 +0300

    scroll

--- src/components/details-popup.tsx
+++ src/components/details-popup.tsx
@@ -19,10 +19,14 @@ const getHref = (link: string): string => {
 
 const useNoScroll = () => {
   useEffect(() => {
+    const root = document.getElementById('root')
+
     document.body.classList.add('no-scroll')
+    root?.classList.add('no-root')
 
     return () => {
       document.body.classList.remove('no-scroll')
+      root?.classList.remove('no-root')
     }
   }, [])
 }
