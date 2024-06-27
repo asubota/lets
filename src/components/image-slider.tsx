0abe98e34e5d34dc702e14564ea9f5e7a0810868
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-06-27 13:04:53 +0300

    img alt, title and broken url

--- src/components/image-slider.tsx
+++ src/components/image-slider.tsx
@@ -7,7 +7,10 @@ import { Box } from '@mui/material'
 import 'swiper/css'
 import 'swiper/css/pagination'
 
-export const ImageSlider: FC<{ pics: string[] }> = ({ pics }) => {
+export const ImageSlider: FC<{ pics: string[]; alt: string }> = ({
+  pics,
+  alt,
+}) => {
   return (
     <Swiper
       modules={[Pagination]}
@@ -19,7 +22,17 @@ export const ImageSlider: FC<{ pics: string[] }> = ({ pics }) => {
       {pics.map((src) => {
         return (
           <SwiperSlide key={src}>
-            <Box component="img" src={src} alt="" sx={{ width: '100%' }} />
+            <Box
+              component="img"
+              src={src}
+              alt={alt}
+              title={alt}
+              sx={{ width: '100%' }}
+              onError={(e) => {
+                const target = e.target as HTMLImageElement
+                target.src = '/lets/logo.webp'
+              }}
+            />
           </SwiperSlide>
         )
       })}
