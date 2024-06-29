710d22b4bfc3bc2b27828b727fe253f8c5b3cacd
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-06-29 21:37:38 +0300

    file id

---
+++ src/store/id.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface StoreState {
+  id: string
+  actions: {
+    setId(this: void, value: string): void
+  }
+}
+
+const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      id: '',
+      actions: {
+        setId: (id) => {
+          set(() => {
+            return { id }
+          })
+        },
+      },
+    }),
+    {
+      name: 'lets-bike-file-id',
+      partialize: (state) => ({ id: state.id }),
+    },
+  ),
+)
+
+export const useIdActions = () => useStore((state) => state.actions)
+export const useBikeId = () => useStore((state) => state.id)
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export * from './table.ts'
 export * from './history.ts'
 export * from './app.ts'
+export * from './id.ts'
