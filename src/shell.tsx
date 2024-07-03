2f965d75458af926d259d4b40a5ed4305b9667ef
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-07-03 11:53:27 +0300

    faster animation and spacing

--- src/shell.tsx
+++ src/shell.tsx
@@ -12,6 +12,7 @@ import { AppBar } from './components/app-bar.tsx'
 import { Scanner } from './components/scanner.tsx'
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form'
 import { SearchForm } from './types.ts'
+import { Box } from '@mui/material'
 
 export const Shell: FC = () => {
   const methods = useForm<SearchForm>({
@@ -57,7 +58,9 @@ export const Shell: FC = () => {
         </FormProvider>
       </AppBar>
 
-      <List list={mode === 'favs' ? favs : list} search={search} />
+      <Box sx={{ p: 1, pt: 0 }}>
+        <List list={mode === 'favs' ? favs : list} search={search} />
+      </Box>
 
       <LimitSearchModal list={list} />
       <TableSettingsModal />
