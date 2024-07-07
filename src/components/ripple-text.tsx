dde618404d6c4b17549a72bbacf58735620b4b17
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-07-07 18:17:01 +0300

    ripple text

---
+++ src/components/ripple-text.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode, SyntheticEvent, useRef } from 'react'
+import TouchRipple, {
+  TouchRippleActions,
+} from '@mui/material/ButtonBase/TouchRipple'
+import { Box } from '@mui/material'
+
+export const RippleText: FC<{ text: ReactNode }> = ({ text }) => {
+  const rippleRef = useRef<TouchRippleActions>(null)
+
+  const handleMouseDown = (event: SyntheticEvent) =>
+    rippleRef.current?.start(event)
+
+  const handleMouseUp = () => rippleRef.current?.stop()
+
+  return (
+    <Box
+      component="span"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      sx={{
+        display: 'inline-block',
+        position: 'relative',
+        cursor: 'pointer',
+        zIndex: 1,
+      }}
+    >
+      {text}
+      <TouchRipple ref={rippleRef} />
+    </Box>
+  )
+}
