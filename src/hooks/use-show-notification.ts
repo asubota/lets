df432372f0250db63ad03723e5797158045bdd91
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-09-06 00:43:02 +0300

    push test 2

---
+++ src/hooks/use-show-notification.ts
@@ -0,0 +1,22 @@
+import { useLinkProps } from '@tanstack/react-router'
+
+export const useShowNotification = (s: string) => {
+  const { href } = useLinkProps({ to: '/', search: { s } })
+  const url = window.origin + href
+
+  return () => {
+    const options: NotificationOptions = {
+      icon: '/lets/logo.webp',
+      body: s,
+      data: { s, url },
+    }
+
+    Notification.requestPermission().then((result) => {
+      if (result === 'granted' && 'serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then((sw) => {
+          return sw.showNotification('Мужчіна!', options)
+        })
+      }
+    })
+  }
+}
