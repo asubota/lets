565f654aa8f18fbc33227f4db9a6c6be27fdea9b
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-09-12 01:03:20 +0300

    add settings icon for tile

---
+++ src/components/tile.tsx
@@ -0,0 +1,145 @@
+import { FC, memo, useState } from 'react'
+import { Product } from '../types.ts'
+import { DetailsPopup } from './details-popup.tsx'
+import { Box, Card, Chip, IconButton } from '@mui/material'
+import { copyContent, getHighlightedText } from '../tools.tsx'
+import { RippleText } from './ripple-text.tsx'
+import { PriceChip } from './price-chip.tsx'
+import { VendorChip } from './vendor-chip.tsx'
+import ImageIcon from '@mui/icons-material/Image'
+import LinkIcon from '@mui/icons-material/Link'
+import StarIcon from '@mui/icons-material/Star'
+import StarBorderIcon from '@mui/icons-material/StarBorder'
+import { HiddenInput } from './hidden-input.tsx'
+import { Stock } from './stock.tsx'
+import { useMatchRoute } from '@tanstack/react-router'
+import SettingsIcon from '@mui/icons-material/Settings'
+
+export const Tile: FC<{
+  p: Product
+  search: string
+  isFav: boolean
+  toggle(): void
+}> = memo(
+  ({ p, search, isFav, toggle }) => {
+    const [details, setDetails] = useState<Product | null>(null)
+    const matchRoute = useMatchRoute()
+    const iFavouriteRoute = !!matchRoute({ to: '/favorites' })
+
+    const handleCardClick = () => {
+      if (p.link || p.pics) {
+        setDetails(p)
+      }
+    }
+
+    return (
+      <>
+        {details && (
+          <DetailsPopup details={details} onClose={() => setDetails(null)} />
+        )}
+        <Card
+          className="product-tile"
+          variant="outlined"
+          sx={{
+            ...(p.missed && {
+              backgroundColor: '#ea2b060f',
+            }),
+            'p': 1,
+            'display': 'grid',
+            'rowGap': '12px',
+            'columnGap': '4px',
+            '&:has(input:checked)': {
+              borderColor: 'text.secondary',
+            },
+            'gridTemplateColumns': '1fr',
+            'gridTemplateRows': 'auto auto auto',
+            'gridTemplateAreas': ` 
+          "name"
+          "sku"
+          "meta"
+        `,
+          }}
+          onClick={handleCardClick}
+        >
+          <Box
+            sx={{
+              gridArea: 'name',
+              textAlign: 'left',
+              wordBreak: 'break-word',
+            }}
+            onClick={async (e) => {
+              e.stopPropagation()
+              await copyContent(p.name)
+            }}
+          >
+            <RippleText text={getHighlightedText(p['name'], search)} />
+          </Box>
+
+          <Chip
+            sx={{ gridArea: 'sku', justifySelf: 'flex-start' }}
+            label={getHighlightedText(p['sku'], search)}
+            size="small"
+            onClick={async (e) => {
+              e.stopPropagation()
+              await copyContent(p.sku)
+            }}
+          />
+
+          <Box
+            sx={{
+              gridArea: 'meta',
+              display: 'flex',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              gap: '8px',
+            }}
+          >
+            <PriceChip product={p} />
+            <VendorChip vendor={p.vendor} />
+
+            {p.pics && (
+              <ImageIcon color="secondary" fontSize="small" data-no-export />
+            )}
+            {p.link && (
+              <LinkIcon color="secondary" fontSize="small" data-no-export />
+            )}
+
+            <Box sx={{ ml: 'auto', display: 'flex', alignItems: 'center' }}>
+              {iFavouriteRoute && (
+                <IconButton
+                  size="small"
+                  sx={{ pt: 0, pb: 0, color: 'secondary.main' }}
+                  data-no-export
+                >
+                  <SettingsIcon />
+                </IconButton>
+              )}
+              <IconButton
+                size="small"
+                data-no-export
+                sx={{
+                  pt: 0,
+                  pb: 0,
+                  color: isFav ? 'warning.light' : 'secondary.light',
+                }}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  toggle()
+                }}
+              >
+                {isFav ? <StarIcon /> : <StarBorderIcon />}
+              </IconButton>
+
+              <HiddenInput>
+                <Stock stock={p.stock} bordered />
+              </HiddenInput>
+            </Box>
+          </Box>
+        </Card>
+      </>
+    )
+  },
+  (p1, p2) => {
+    return p1.isFav === p2.isFav && p1.search === p2.search
+  },
+)
--- src/store/app.ts
+++ src/store/app.ts
@@ -3,13 +3,10 @@ import { persist } from 'zustand/middleware'
 
 interface StoreState {
   view: 'tile' | 'table'
-  mode: 'search' | 'favs'
   theme: 'dark' | 'light'
   actions: {
-    setMode(this: void, mode: StoreState['mode']): void
     setView(this: void, view: StoreState['view']): void
     setTheme(this: void, theme: StoreState['theme']): void
-    toggleFavs(this: void): void
   }
 }
 
@@ -21,19 +18,13 @@ const useStore = create<StoreState>()(
       theme: 'light',
       actions: {
         setView: (view) => set(() => ({ view })),
-        setMode: (mode) => set(() => ({ mode })),
         setTheme: (theme) => set({ theme }),
-        toggleFavs: () =>
-          set((state) => {
-            return { mode: state.mode === 'favs' ? 'search' : 'favs' }
-          }),
       },
     }),
     {
       name: 'lets-bike-app',
       partialize: (state) => ({
         view: state.view,
-        mode: state.mode,
         theme: state.theme,
       }),
     },
@@ -42,5 +33,4 @@ const useStore = create<StoreState>()(
 
 export const useAppActions = () => useStore((state) => state.actions)
 export const useAppView = () => useStore((state) => state.view)
-export const useAppMode = () => useStore((state) => state.mode)
 export const useAppTheme = () => useStore((state) => state.theme)
