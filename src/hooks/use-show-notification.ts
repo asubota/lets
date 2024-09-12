22a9481f45bfd48db75d84dfd0d7f83756ab7cd7
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-09-12 17:05:16 +0300

    show notifications

--- src/hooks/use-show-notification.ts
+++ src/hooks/use-show-notification.ts
@@ -1,19 +1,15 @@
-import { useLinkProps } from '@tanstack/react-router'
-
-export const useShowNotification = (s: string) => {
-  const { href: to } = useLinkProps({ to: '/', search: { s } })
-
-  return () => {
+export const useShowNotification = () => {
+  return (title: string, body: string) => {
     const options: NotificationOptions = {
       icon: '/lets/logo.webp',
-      body: s,
-      data: { s, to },
+      body,
+      data: { s: '', to: '' },
     }
 
     Notification.requestPermission().then((result) => {
       if (result === 'granted' && 'serviceWorker' in navigator) {
         navigator.serviceWorker.ready.then((sw) => {
-          return sw.showNotification('Мущіна, ХУЙ!', options)
+          return sw.showNotification(title, options)
         })
       }
     })
--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,12 +1,19 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { Container } from '@mui/material'
 import { AppBar } from '../components/app-bar.tsx'
+import { useNotifyAboutChange } from '../hooks/use-notify-about-change.ts'
 
-export const Route = createFileRoute('/_layout')({
-  component: () => (
+const Component = () => {
+  useNotifyAboutChange()
+
+  return (
     <Container sx={{ pl: 1, pr: 1, pb: 1 }}>
       <AppBar />
       <Outlet />
     </Container>
-  ),
+  )
+}
+
+export const Route = createFileRoute('/_layout')({
+  component: Component,
 })
