0e54fcc6ffb67dd08c218540671f489c70ecb342
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-09-17 16:06:41 +0300

    re-fetch on cache change

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -2,9 +2,11 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 import { Container } from '@mui/material'
 import { AppBar } from '../components/app-bar.tsx'
 import { useNotifyAboutChange } from '../hooks/use-notify-about-change.ts'
+import { useListenToCacheUpdate } from '../hooks/use-listen-to-cache-update.ts'
 
 const Component = () => {
   useNotifyAboutChange()
+  useListenToCacheUpdate()
 
   return (
     <Container sx={{ pl: 1, pr: 1, pb: 1 }}>
