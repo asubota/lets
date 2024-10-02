57dd16b7268bd213260c438e2e10278dd021f6ed
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-10-02 19:00:43 +0300

    implement click to search button

---
+++ src/components/toolbar/paste-in-search-button.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import { IconButton } from '@mui/material'
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch'
+import { useNavigate } from '@tanstack/react-router'
+
+export const PasteInSearchButton: FC = () => {
+  const navigate = useNavigate()
+
+  const handleClick = async () => {
+    const text = await navigator.clipboard.readText()
+    await navigate({ to: '/', search: { s: text } })
+  }
+
+  return (
+    <IconButton
+      size="small"
+      sx={{ color: 'text.secondary', mr: '20px' }}
+      onClick={handleClick}
+    >
+      <ContentPasteSearchIcon />
+    </IconButton>
+  )
+}
