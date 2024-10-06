0ee9947835fd8e4b9ca627a8517b25e592568470
beregovoy <1@1>
2024-10-06 14:35:52 +0300

    google fix

--- src/components/bike.tsx
+++ src/components/bike.tsx
@@ -1,11 +1,32 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Box } from '@mui/material'
 import SafeBike from './bike.svg?react'
 import BrokenBike from './broken-bike.svg?react'
+import { loadGoogleApi, signIn } from '../google-auth.ts'
+import { setGoogleAuthToken } from '../secrets.ts'
 
 export const Bike: FC<{ type: 'safe' | 'broken' }> = ({ type }) => {
+  const [loading, setLoading] = useState(false)
+
+  const handleClick = () => {
+    setLoading(true)
+
+    loadGoogleApi().then(async () => {
+      const authInstance = gapi.auth2.getAuthInstance()
+
+      if (!authInstance.isSignedIn.get()) {
+        const accessToken = await signIn()
+
+        setGoogleAuthToken(accessToken)
+      }
+
+      setLoading(false)
+    })
+  }
+
   return (
     <Box
+      onClick={handleClick}
       sx={{
         width: '100%',
         maxWidth: '300px',
@@ -13,6 +34,9 @@ export const Bike: FC<{ type: 'safe' | 'broken' }> = ({ type }) => {
         bottom: '185px',
         left: '50%',
         transform: 'translate(-50%)',
+        cursor: loading ? 'none' : 'pointer',
+        borderRadius: '50%',
+        backgroundColor: loading ? 'secondary.main' : 'transparent',
       }}
     >
       {type === 'safe' && <SafeBike />}
