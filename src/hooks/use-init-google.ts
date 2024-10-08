f79d136d02f5ce8843920e0d12beb3ff60d6bd61
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2024-10-08 21:12:24 +0300

    google in 2

--- src/hooks/use-init-google.ts
+++ src/hooks/use-init-google.ts
@@ -1,8 +1,20 @@
 import { useEffect } from 'react'
 import { loadGoogleApi, initGoogleAuth } from '../google-auth.ts'
+import { enqueueSnackbar } from 'notistack'
+
+const showAlert = () => {
+  enqueueSnackbar(`Google Off`, {
+    variant: 'error',
+    autoHideDuration: 2000,
+    anchorOrigin: {
+      vertical: 'top',
+      horizontal: 'center',
+    },
+  })
+}
 
 export const useInitGoogle = () => {
   useEffect(() => {
-    loadGoogleApi().then(initGoogleAuth)
+    loadGoogleApi().then(initGoogleAuth).catch(showAlert)
   }, [])
 }
