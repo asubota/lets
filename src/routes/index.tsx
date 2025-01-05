6ca9ae6cf064f87d6e097ce3836174c153a676b3
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-01-05 18:30:46 +0200

    better routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Home } from '../areas/home/home.tsx'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: Home,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/list',
+    })
+  },
+  notFoundComponent: () => {
+    console.log('Not Found')
+  },
 })
