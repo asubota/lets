88deb5441eacab0d3a6409f76b8224323a800f26
asubota <asubota@users.noreply.github.com>
2025-01-14 13:53:43 +0000

    deploy: 45f3cf873d01ccc8871976cdb1ed8bc50f6bbfb1

---
+++ assets/scanner.lazy-8-VtkQc1.js
@@ -0,0 +1,1 @@
+import{s as o}from"./tanstack-DrrzDSOT.js";import{S as r}from"./index-CvWbQ8Kj.js";import"./react-_uqhELNW.js";import"./mui-DpcCcPuz.js";import"./html2canvas-BfYXEYrK.js";import"./tesseract-efYOn2sj.js";import"./swiper-B72pzicN.js";const c=o("/scanner")({component:r});export{c as Route};
