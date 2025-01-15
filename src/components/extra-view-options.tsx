f3cab610f11d3ad4b9e3c1845994a0333c98af43
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-01-15 11:48:14 +0200

    cart items numbers

--- src/components/extra-view-options.tsx
+++ src/components/extra-view-options.tsx
@@ -1,6 +1,6 @@
 import { FC, useContext } from 'react'
 import PaletteIcon from '@mui/icons-material/Palette'
-import { IconButton, Stack, useTheme } from '@mui/material'
+import { Box, IconButton, Stack, useTheme } from '@mui/material'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 import NotificationsIcon from '@mui/icons-material/Notifications'
@@ -11,10 +11,12 @@ import { ColorModeContext } from '../theme-mode-provider.tsx'
 import { createLink } from '@tanstack/react-router'
 import { RedDot } from './red-dot.tsx'
 import { useGetNotifications } from '../hooks/use-get-notifications.ts'
+import { useCartItemsCount } from '../hooks/use-cart-items-count.ts'
 
-const LinkedButton = createLink(IconButton)
+const LinkedIconButton = createLink(IconButton)
 
 export const ExtraViewOptions: FC = () => {
+  const count = useCartItemsCount()
   const theme = useTheme()
   const colorMode = useContext(ColorModeContext)
   const unread = useGetNotifications('unread')
@@ -29,10 +31,10 @@ export const ExtraViewOptions: FC = () => {
           bottom: '25px',
         }}
       >
-        <LinkedButton sx={{ color: 'text.secondary' }} to="/notifications">
+        <LinkedIconButton sx={{ color: 'text.secondary' }} to="/notifications">
           <NotificationsIcon />
           {unread.length > 0 && <RedDot />}
-        </LinkedButton>
+        </LinkedIconButton>
 
         <IconButton
           onClick={colorMode.toggleColorMode}
@@ -45,13 +47,13 @@ export const ExtraViewOptions: FC = () => {
           )}
         </IconButton>
 
-        <LinkedButton sx={{ color: 'text.secondary' }} to="/colors">
+        <LinkedIconButton sx={{ color: 'text.secondary' }} to="/colors">
           <PaletteIcon />
-        </LinkedButton>
+        </LinkedIconButton>
 
-        <LinkedButton sx={{ color: 'text.secondary' }} to="/stats">
+        <LinkedIconButton sx={{ color: 'text.secondary' }} to="/stats">
           <InsightsIcon />
-        </LinkedButton>
+        </LinkedIconButton>
       </Stack>
 
       <Stack
@@ -62,9 +64,21 @@ export const ExtraViewOptions: FC = () => {
           bottom: '25px',
         }}
       >
-        <LinkedButton to="/cart" sx={{ color: 'text.secondary' }}>
+        <LinkedIconButton to="/cart" sx={{ color: 'text.secondary' }}>
           <ShoppingCartIcon />
-        </LinkedButton>
+          <Box
+            component="span"
+            sx={{
+              color: 'red',
+              fontSize: '12px',
+              position: 'absolute',
+              right: '-5px',
+              top: '-2px',
+            }}
+          >
+            {count}
+          </Box>
+        </LinkedIconButton>
       </Stack>
     </>
   )
