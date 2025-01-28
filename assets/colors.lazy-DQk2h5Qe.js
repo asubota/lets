5551d2b3ad6bfa5e9a1b0a529017a0a8b222cccb
asubota <asubota@users.noreply.github.com>
2025-01-28 21:20:46 +0000

    deploy: 4c259e5310999da0f53af13a9cbf97ba7871947f

--- assets/colors.lazy-DQk2h5Qe.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./tanstack-jm6MSW9M.js";import{C as t}from"./index-DLkTqRN4.js";import"./react-_uqhELNW.js";import"./mui-C3GIU0Fs.js";import"./html2canvas-BfYXEYrK.js";import"./tesseract-DOmqAGAa.js";import"./swiper-BQVCGc5u.js";const c=o("/colors")({component:t});export{c as Route};
