2587d0d3c0202da993f3cb7ca8aa87bd6ec12a04
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-01-28 20:53:25 +0300

    add some effect

--- src/components/bike.tsx
+++ src/components/bike.tsx
@@ -1,10 +1,26 @@
-import { FC } from 'react'
-import { Box } from '@mui/material'
+import { Box, ToggleButton, ToggleButtonGroup } from '@mui/material'
 import SafeBike from './bike.svg?react'
 import BrokenBike from './broken-bike.svg?react'
 import { Version } from './version.tsx'
+import WbSunnyIcon from '@mui/icons-material/WbSunny'
+import InvertColorsIcon from '@mui/icons-material/InvertColors'
+import FormatColorTextIcon from '@mui/icons-material/FormatColorText'
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed'
+import { useState } from 'react'
 
-export const Bike: FC<{ type: 'safe' | 'broken' }> = ({ type }) => {
+const allEffects = ['bnw', 'invert', 'glow', 'gradient'] as const
+type Effect = (typeof allEffects)[number]
+
+export const Bike = ({ type }: { type: 'safe' | 'broken' }) => {
+  const [effects, setEffects] = useState<Effect[]>([])
+
+  const handle = (_: unknown, newEffects: Effect[]) => {
+    setEffects(newEffects)
+
+    allEffects.forEach((e) => document.body.classList.remove(e))
+    newEffects.forEach((effect) => document.body.classList.add(effect))
+  }
+
   return (
     <Box
       sx={{
@@ -18,7 +34,27 @@ export const Bike: FC<{ type: 'safe' | 'broken' }> = ({ type }) => {
     >
       {type === 'safe' && <Version />}
       {type === 'safe' && <SafeBike />}
-      {type === 'broken' && <BrokenBike />}
+      {type === 'broken' && (
+        <>
+          <BrokenBike />
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: '20px' }}>
+            <ToggleButtonGroup value={effects} onChange={handle} size="small">
+              <ToggleButton value="bnw">
+                <WbSunnyIcon fontSize="small" />
+              </ToggleButton>
+              <ToggleButton value="invert">
+                <InvertColorsIcon fontSize="small" />
+              </ToggleButton>
+              <ToggleButton value="glow">
+                <FormatColorTextIcon fontSize="small" />
+              </ToggleButton>
+              <ToggleButton value="gradient">
+                <DynamicFeedIcon fontSize="small" />
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+        </>
+      )}
     </Box>
   )
 }
