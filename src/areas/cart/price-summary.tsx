4bb2aa8cda8d7bb9b92c7c53f0f0a86d0fb522ff
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-01-31 14:18:00 +0300

    cart summary

--- src/areas/cart/price-summary.tsx
+++ src/areas/cart/price-summary.tsx
@@ -1,17 +1,40 @@
-import { FC } from 'react'
 import { Box } from '@mui/material'
+import { findProduct } from '../../tools.tsx'
+import { useGetCart } from '../../cart-api.ts'
+import { useAllData } from '../../use-data.ts'
 
-interface PriceSummaryProps {
-  totalDiscount: number
-  fullPrice: number
-  discountPrice: number
-}
+export const PriceSummary = () => {
+  const data = useAllData()
+  const { data: cartItems = [] } = useGetCart()
+  const fullData = cartItems.map((item) => {
+    return { ...item, product: findProduct(item.itemId, data) }
+  })
 
-export const PriceSummary: FC<PriceSummaryProps> = ({
-  fullPrice,
-  discountPrice,
-  totalDiscount,
-}) => {
+  const fullPrice = fullData.reduce((acc, item) => {
+    const price = item.product?.price || 0
+    const quantity = parseInt(item.quantity, 10)
+
+    return acc + price * quantity
+  }, 0)
+
+  const discountPrice = fullData.reduce((acc, item) => {
+    const price = item.product?.price || 0
+    const quantity = parseInt(item.quantity, 10)
+    const discount = parseInt(item.discount, 10)
+    const discountedPrice = price - Math.ceil((price * discount) / 100)
+
+    return acc + discountedPrice * quantity
+  }, 0)
+
+  const totalDiscount = fullData.reduce((acc, item) => {
+    const price = item.product?.price || 0
+    const quantity = parseInt(item.quantity, 10)
+    const discount = parseInt(item.discount, 10)
+    const discountPrice = Math.ceil((price * discount) / 100)
+
+    return acc + discountPrice * quantity
+  }, 0)
+
   return (
     <>
       {totalDiscount > 0 && (
