362c07335f8f61d2a35f4ec2c3bf644dcbaa4bbb
asubota <asubota@users.noreply.github.com>
2025-01-31 12:26:27 +0000

    deploy: 8dbb11e511bcd5f3c7995e5efd39c36367e0f083

--- assets/notifications.lazy-BnhDSFr0.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./tanstack-jm6MSW9M.js";import{N as t}from"./index-D5pDt9C6.js";import"./react-_uqhELNW.js";import"./mui-GtXf8lzG.js";import"./html2canvas-BfYXEYrK.js";import"./tesseract-DOmqAGAa.js";import"./swiper-BQVCGc5u.js";const s=o("/notifications")({component:t});export{s as Route};
