d5d829dc96a71fcfeca179c437be7c6ddab9ed07
aZmOine <aZmOine@users.noreply.github.com>
2025-04-06 14:52:55 +0000

    deploy: 6b032fd38d3bf9ef5d1bd81edcb00c71e2ade633

---
+++ assets/scanner.lazy-CPr5fs5P.js
@@ -0,0 +1,1 @@
+import{s as o}from"./tanstack-jm6MSW9M.js";import{S as r}from"./index-C0GxLMUC.js";import"./react-_uqhELNW.js";import"./mui-DGAJCQd1.js";import"./html2canvas-BfYXEYrK.js";import"./tesseract-DOmqAGAa.js";import"./swiper-BQVCGc5u.js";const c=o("/scanner")({component:r});export{c as Route};
