35132056789fd7d8c35e6dcf4d9ea292e919bf6c
Lets Bike <letsbike.kiev@gmail.com>
2025-04-06 14:53:42 +0300

    pics on favs

--- src/areas/list/details.tsx
+++ src/areas/list/details.tsx
@@ -1,18 +1,18 @@
-import { FC } from 'react'
 import { DetailsPopup } from '../../components/details-popup.tsx'
-import { Navigate, useNavigate, useParams } from '@tanstack/react-router'
+import { Navigate } from '@tanstack/react-router'
 import { useGetById } from './use-get-by-id.ts'
 
-export const Details: FC = () => {
-  const { id } = useParams({ from: '/_layout/list/$id/details' })
+interface DetailsProps {
+  id: string
+  onClose(): void
+}
+
+export const Details = ({ id, onClose }: DetailsProps) => {
   const product = useGetById(id)
-  const navigate = useNavigate()
 
   if (!product || !product.pics) {
     return <Navigate to="/list" />
   }
 
-  return (
-    <DetailsPopup details={product} onClose={() => navigate({ to: '/list' })} />
-  )
+  return <DetailsPopup details={product} onClose={onClose} />
 }
