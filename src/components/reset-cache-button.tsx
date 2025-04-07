895a0acaeddb66b1421799533f68068e7a363dc8
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-04-07 12:20:37 +0300

    reset cache button works

--- src/components/reset-cache-button.tsx
+++ src/components/reset-cache-button.tsx
@@ -1,13 +1,26 @@
 import CachedIcon from '@mui/icons-material/Cached'
 import { IconButton } from '@mui/material'
 import { useState } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import { CACHE_BASE_KEY } from '../constants.ts'
 
 export const ResetCacheButton = () => {
   const [rotating, setRotating] = useState(false)
+  const queryClient = useQueryClient()
 
   const handleClick = () => {
     setRotating(true)
     setTimeout(() => setRotating(false), 1200)
+
+    queryClient.invalidateQueries({ queryKey: [CACHE_BASE_KEY] }).then(() => {
+      navigator.serviceWorker.ready.then(() => {
+        if (navigator.serviceWorker.controller) {
+          navigator.serviceWorker.controller.postMessage({
+            type: 'reset-cache',
+          })
+        }
+      })
+    })
   }
 
   return (
