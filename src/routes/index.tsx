133dbc80b4f0df4ee10a55c676bb47351236b79a
Oleksandr Subota <oleksandr@MacBook-Pro-Oleksandr.local>
2025-04-30 22:25:19 +0300

    update libs & minify sw

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  beforeLoad: () => {
-    throw redirect({
-      to: '/list',
-    })
+  beforeLoad() {
+    throw redirect({ to: '/list' })
   },
 })
