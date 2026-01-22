c04011cf32806b96be17b6878c9ed5d4ba96f96b
asubota <asubota@users.noreply.github.com>
2026-01-22 16:08:33 +0000

    deploy: 3a4fb096e2c666cb23a4e02ba60aa9746949b144

---
+++ assets/House-CgaQxdzk.js
@@ -0,0 +1,1 @@
+import{j as o}from"./tanstack-BOxH6YKw.js";import{d as s}from"./mui-DIb8rZxV.js";const r=s(o.jsx("path",{d:"M19 9.3V4h-3v2.6L12 3 2 12h3v8h5v-6h4v6h5v-8h3zm-9 .7c0-1.1.9-2 2-2s2 .9 2 2z"}));export{r as H};
